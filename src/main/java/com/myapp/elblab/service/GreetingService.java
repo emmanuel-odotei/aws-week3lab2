a61adef900bfa827017f319dd807a943b3550e79
Emmanuel Odoi Odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-06-08 15:09:36 +0000

    github actions workflow added for s3 upload

--- src/main/java/com/myapp/elblab/service/GreetingService.java
+++ src/main/java/com/myapp/elblab/service/GreetingService.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 @Service
 public class GreetingService {
+    
     private static final List<String> EMOJIS = List.of("🚀", "🌟", "✨", "💡", "🔥", "🎉", "😎", "👋", "🧠");
     private final Random random = new Random();
     
