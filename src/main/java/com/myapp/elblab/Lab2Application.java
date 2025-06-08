8395d28641af40c33bc35d7e422cb8d2e740d7fa
Emmanuel Odoi Odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-06-08 14:35:58 +0000

    added controllers and services

--- src/main/java/com/myapp/elblab/Lab2Application.java
+++ src/main/java/com/myapp/elblab/Lab2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElblabApplication {
+public class Lab2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElblabApplication.class, args);
+		SpringApplication.run( Lab2Application.class, args);
 	}
 
 }
