2e5bc35b0a8250a9f09feff5d9ced0160726c9cc
Emmanuel Odoi Odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-06-08 14:03:56 +0000

    initial commit

---
+++ src/main/java/com/myapp/elblab/ElblabApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.elblab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElblabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElblabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/elblab/ElblabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.elblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElblabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
